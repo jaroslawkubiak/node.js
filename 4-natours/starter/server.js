87aeefa0a4e90172f06cd763540818ff5cdee516
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2023-09-25 19:26:04 +0200

    finish section 7 - creating and connecting do mongoDB

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,20 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB conn success');
+  });
 
 // 4) START server
 const port = process.env.PORT || 3000;
