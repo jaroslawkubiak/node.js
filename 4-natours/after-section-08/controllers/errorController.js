0c7eab737e8b7854f6b13b545e492645160dfe39
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2023-09-28 20:39:03 +0200

    finish section 9 - handling errors

--- 4-natours/after-section-08/controllers/errorController.js
+++ 4-natours/after-section-08/controllers/errorController.js
@@ -1,3 +1,24 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -14,26 +35,36 @@ const sendErrorProd = (err, res) => {
       status: err.status,
       message: err.message
     });
-    // Programing or other unknown: don't leak error details
+
+    // Programming or other unknown error: don't leak error details
   } else {
-    // 1) Log error to console
-    console.error('Error 🧨🧨🧨', err);
+    // 1) Log error
+    console.error('ERROR 💥', err);
 
     // 2) Send generic message
     res.status(500).json({
-      status: 'error',
-      message: 'something went very wrong!'
+      status: 'Error',
+      message: 'Something went very wrong!'
     });
   }
 };
 
 module.exports = (err, req, res, next) => {
+  // console.log(err.stack);
+
   err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'Error🔥';
+  err.status = err.status || 'Error';
 
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 };
